855ff5f15648b6758363f33800dfdee3b636916a
sangwoo <33356618+gomtings@users.noreply.github.com>
2021-07-19 20:05:32 +0900

    revent gear not D Event

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -107,9 +107,9 @@ class CarInterfaceBase():
       events.add(EventName.doorOpen)
     if cs_out.seatbeltUnlatched:
       events.add(EventName.seatbeltNotLatched)
- #   if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
-  #     cs_out.gearShifter not in extra_gears):
- #     events.add(EventName.wrongGear)
+    if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
+       cs_out.gearShifter not in extra_gears):
+      events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
     if not cs_out.cruiseState.available:
